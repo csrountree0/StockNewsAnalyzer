1996ed9f045a4ce98f40da2729b1137ff05b36ab
csrountree0 <crountree3706@floridapoly.edu>
2025-05-26 18:34:13 -0400

    only show same day news since performing sentiment analysis on all these news articles would take a long time and i also dont feel they are useful

--- src/App.jsx
+++ src/App.jsx
@@ -34,17 +34,18 @@ function App() {
       // calculate date ranges
       const endDate = new Date(date)
       const startDate = new Date(date)
-      startDate.setDate(startDate.getDate() - 5) // get price and news 5 days prior for better visualization
+      startDate.setDate(startDate.getDate() - 5) // get price 5 days prior for better visualization
       const oneYearLater = new Date(date)
       oneYearLater.setFullYear(oneYearLater.getFullYear() + 1)
 
       // fetch stock prices and news
       const [stockResult, newsResult] = await Promise.all([
         fetchStockPrice(ticker, startDate, oneYearLater),
-        fetchNews(ticker, startDate, endDate)
+        fetchNews(ticker, endDate, endDate)
       ]);
       setStockData(stockResult)
       setNewsData(newsResult)
+      console.log(newsResult)
       setErrors({ ticker: '', date: '' })
     } catch (error) {
       console.error('Error:', error)
@@ -62,21 +63,24 @@ function App() {
       {/* Header */}
       <header className={`${isDark ? 'bg-gray-800' : 'bg-white'} shadow-sm sticky top-0 z-10`}>
         <div className="max-w-7xl mx-auto py-3 px-3 sm:py-4 sm:px-4 flex justify-between items-center">
+          <div className="flex-1"></div>
           <h1 className={`text-xl sm:text-2xl md:text-3xl font-bold cursor-default ${isDark ? 'text-white' : 'text-gray-900'}`}>Stock News Analyzer</h1>
-          <button
-            onClick={() => setIsDark(!isDark)}
-            className="p-1.5 sm:p-2 rounded-lg cursor-pointer"
-          >
-            {isDark ? (
-              <svg className="w-5 h-5 sm:w-6 sm:h-6 text-yellow-500 fill-yellow-500 hover:fill-none stroke-yellow-500" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-              </svg>
-            ) : (
-              <svg className="w-5 h-5 sm:w-6 sm:h-6 text-gray-700 fill-none stroke-gray-700 hover:fill-gray-700 hover:stroke-none" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-              </svg>
-            )}
-          </button>
+          <div className="flex-1 flex justify-end">
+            <button
+              onClick={() => setIsDark(!isDark)}
+              className="p-1.5 sm:p-2 rounded-lg cursor-pointer"
+            >
+              {isDark ? (
+                <svg className="w-5 h-5 sm:w-6 sm:h-6 text-yellow-500 fill-yellow-500 hover:fill-none stroke-yellow-500" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                </svg>
+              ) : (
+                <svg className="w-5 h-5 sm:w-6 sm:h-6 text-gray-700 fill-none stroke-gray-700 hover:fill-gray-700 hover:stroke-none" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
       </header>
 
